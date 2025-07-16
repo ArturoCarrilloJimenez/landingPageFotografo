cc40f98124de32a6591ed4e7d36ab3df110ba4d2
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2025-07-16 13:21:13 +0200

    add: mod hero, style an font, add btn whatsapp

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "./templates/hero/hero";
 import { InfiniteLoop } from "./templates/infinite-loop/infinite-loop";
+import { BtnWhatsappComponent } from "../shared/templates/btn-whatsapp/btn-whatsapp.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Hero, InfiniteLoop],
+  imports: [Hero, InfiniteLoop, BtnWhatsappComponent],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss'
 })
---
+++ src/app/shared/templates/btn-whatsapp/btn-whatsapp.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-btn-whatsapp',
+  imports: [],
+  templateUrl: './btn-whatsapp.html',
+  styleUrl: './btn-whatsapp.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BtnWhatsappComponent { }
