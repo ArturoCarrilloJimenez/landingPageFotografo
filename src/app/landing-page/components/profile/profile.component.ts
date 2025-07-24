32af72c649b997ed3bef0262ed662877835bea93
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2025-07-24 21:07:45 +0200

    prueba optimizar web

--- src/app/landing-page/components/profile/profile.component.ts
+++ src/app/landing-page/components/profile/profile.component.ts
@@ -1,14 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-
-interface PortfolioItem {
-  src: string;
-  alt: string;
-  title: string;
-  description: string;
-  category: string;
-}
+import { portfolioItems } from './data/profileData';
 
 @Component({
   selector: 'landing-page-profile',
@@ -23,92 +16,7 @@ export class ProfileComponent {
   messageContact =
     'Hola, he visto tu perfil y me gustaría contactarte para hablar sobre un proyecto.';
 
-  portfolioItems: PortfolioItem[] = [
-    {
-      src: 'images/destacadas/individual/chica_concierto_v.webp',
-      alt: 'Artista en concierto con luz dramática y enfoque en el rostro.',
-      title: 'Artista en concierto con luz dramática y enfoque en el rostro.',
-      description: 'Captura de un artista en concierto, resaltando la emoción del momento con luz dramática.',
-      category: 'Concierto',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-  ];
+  portfolioItems = portfolioItems;
 
   showMoreItems = () => {
     this.visibleItems.update((v) => v + 3);
