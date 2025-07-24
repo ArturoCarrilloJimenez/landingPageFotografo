b88058821a3e829fa4bbadf43ca8687e2187b4f2
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2025-07-24 21:37:06 +0200

    rollback: vualta a la version anterior, esta es estable pero esta muy mal optimizada, tiene bastantes tirones con tantas imágenes

--- src/app/landing-page/components/profile/profile.component.ts
+++ src/app/landing-page/components/profile/profile.component.ts
@@ -1,19 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-<<<<<<< HEAD
 import { portfolioItems } from './data/profileData';
-=======
-
-interface PortfolioItem {
-  id: number;
-  src: string;
-  alt: string;
-  title: string;
-  description: string;
-  category: string;
-}
->>>>>>> parent of 35088aa (add: selección de imágenes)
 
 @Component({
   selector: 'landing-page-profile',
@@ -27,108 +15,7 @@ export class ProfileComponent {
   urlContact = environment.urlContact;
   messageContact = 'Hola, he visto tu perfil y me gustaría contactarte para hablar sobre un proyecto.';
 
-<<<<<<< HEAD
   portfolioItems = portfolioItems;
-=======
-  portfolioItems: PortfolioItem[] = [
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-  ];
->>>>>>> parent of 35088aa (add: selección de imágenes)
 
   showMoreItems = () => {
     this.visibleItems.update((v) => v + 3);
