35088aa36a251e57f5ecd466dce28cc7d628506f
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2025-07-24 19:43:22 +0200

    add: selección de imágenes

--- src/app/landing-page/components/profile/profile.component.ts
+++ src/app/landing-page/components/profile/profile.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
 
 interface PortfolioItem {
-  id: number;
   src: string;
   alt: string;
   title: string;
@@ -21,11 +20,18 @@ interface PortfolioItem {
 export class ProfileComponent {
   visibleItems = signal(6);
   urlContact = environment.urlContact;
-  messageContact = 'Hola, he visto tu perfil y me gustaría contactarte para hablar sobre un proyecto.';
+  messageContact =
+    'Hola, he visto tu perfil y me gustaría contactarte para hablar sobre un proyecto.';
 
   portfolioItems: PortfolioItem[] = [
     {
-      id: 1,
+      src: 'images/destacadas/individual/chica_concierto_v.webp',
+      alt: 'Artista en concierto con luz dramática y enfoque en el rostro.',
+      title: 'Artista en concierto con luz dramática y enfoque en el rostro.',
+      description: 'Captura de un artista en concierto, resaltando la emoción del momento con luz dramática.',
+      category: 'Concierto',
+    },
+    {
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -33,7 +39,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -41,7 +46,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -49,7 +53,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -57,7 +60,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -65,7 +67,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -73,7 +74,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -81,7 +81,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -89,7 +88,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -97,7 +95,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
@@ -105,15 +102,6 @@ export class ProfileComponent {
       category: 'Retrato',
     },
     {
-      id: 1,
-      src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
-      alt: 'Imagen 1',
-      title: 'Título 1',
-      description: 'Descripción 1',
-      category: 'Retrato',
-    },
-    {
-      id: 1,
       src: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
       alt: 'Imagen 1',
       title: 'Título 1',
